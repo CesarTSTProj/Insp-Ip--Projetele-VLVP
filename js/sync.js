98a7212627e88f3994e77bc92ade068c78423f84
CTRLVV <cesaraugustoleitesilveira@gmail.com>
2026-08-19 14:44:53 -0300

    Create sync.js with initial comments and responsibilities
    Add initial documentation for sync.js

---
+++ js/sync.js
@@ -0,0 +1,16 @@
+/*
+=================================================
+INSP - SINCRONIZAÇÃO
+=================================================
+
+Responsabilidades:
+
+- Preparar dados para envio
+- Converter foto para Base64
+- Comunicar com Google Apps Script
+- Sincronizar inspeções pendentes
+- Sincronização manual
+- Validar resposta do servidor
+
+=================================================
+*/
